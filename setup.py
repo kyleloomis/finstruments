7a5b31be92b3e1878e53ce5aaf59a14dc03d50e5
Kyle Loomis <kyle@spotlight.dev>
2024-10-17 22:05:04 -0700

    Feature [ENG-000] get_tag fixes

--- setup.py
+++ setup.py
@@ -19,32 +19,34 @@ except ImportError:
 
 def get_tag():
     try:
-        # Attempt to get the latest tag
-        tag = subprocess.run(
-            ["git", "describe", "--tags", "--abbrev=0"],
-            check=True,
-            text=True,
-            capture_output=True,
-        ).stdout.strip()
-
-        # Remove the 'v' prefix if it exists
-        if tag.startswith("v"):
-            tag = tag[1:]
-
-        # Verify that the tag is in a valid format
-        if not tag or not tag[0].isdigit():
-            raise ValueError(f"Invalid tag format: {tag}")
-
-        # Count the number of commits since the last tag
-        commits_since_tag = subprocess.run(
-            ["git", "rev-list", f"{tag}..HEAD", "--count"],
-            check=True,
-            text=True,
-            capture_output=True,
-        ).stdout.strip()
-
-        # Form the new version
-        base_version = tag
+        # TODO
+        # # Attempt to get the latest tag
+        # tag = subprocess.run(
+        #     ["git", "describe", "--tags", "--abbrev=0"],
+        #     check=True,
+        #     text=True,
+        #     capture_output=True,
+        # ).stdout.strip()
+        #
+        # # Remove the 'v' prefix if it exists
+        # if tag.startswith("v"):
+        #     tag = tag[1:]
+        #
+        # # Verify that the tag is in a valid format
+        # if not tag or not tag[0].isdigit():
+        #     raise ValueError(f"Invalid tag format: {tag}")
+        #
+        # # Count the number of commits since the last tag
+        # commits_since_tag = subprocess.run(
+        #     ["git", "rev-list", f"{tag}..HEAD", "--count"],
+        #     check=True,
+        #     text=True,
+        #     capture_output=True,
+        # ).stdout.strip()
+        #
+        # # Form the new version
+        # base_version = tag
+        base_version = "0.1"
         new_version = f"{base_version}.{commits_since_tag}"
 
         return new_version
